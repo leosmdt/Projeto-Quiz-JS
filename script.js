c8792237e612e796a3ca859fee24be0542f82e80
Leonardo Schmidt <leonardosch2000@hotmail.com>
2023-03-24 09:58:43 -0300

    Subindo arquivos do projeto

---
+++ script.js
@@ -0,0 +1,72 @@
+// initial data
+let currentQuestion = 0
+let correctAnswer = 0
+showQuestion()
+
+// functions
+function showQuestion() {
+  if (questions[currentQuestion]) {
+    let q = questions[currentQuestion]
+
+    let pct = Math.floor((currentQuestion / questions.length) * 100)
+
+    document.querySelector('.progress--bar').style.width = `${pct}%`
+
+    document.querySelector('.scoreArea').style.display = 'none'
+    document.querySelector('.questionArea').style.display = 'block'
+    document.querySelector('.question').innerHTML = q.question
+    document.querySelector('.options').innerHTML = ''
+
+    let optionsHtml = ''
+    for (let i in q.options) {
+      optionsHtml += `<div data-op="${i}"class="option"><span>${
+        parseInt(i) + 1
+      }</span>${q.options[i]}</div>`
+    }
+
+    document.querySelector('.options').innerHTML = optionsHtml
+
+    document.querySelectorAll('.options .option').forEach(item => {
+      item.addEventListener('click', optionClickEvent)
+    })
+  } else {
+    finishQuiz()
+  }
+}
+
+function optionClickEvent(e) {
+  let clickedOption = parseInt(e.target.getAttribute('data-op'))
+  if (questions[currentQuestion].answer === clickedOption) {
+    correctAnswer++
+  }
+  currentQuestion++
+  showQuestion()
+}
+
+function finishQuiz() {
+  let points = Math.floor((correctAnswer / questions.length) * 100)
+  document.querySelector('.scorePct').innerHTML = `Acertou ${points}%`
+  document.querySelector(
+    '.scoreText2'
+  ).innerHTML = `Você respondeu ${questions.length} questões e acertou ${correctAnswer}`
+  document.querySelector('.questionArea').style.display = 'none'
+  document.querySelector('.scoreArea').style.display = 'block'
+  document.querySelector('.progress--bar').style.width = '100%'
+  if (correctAnswer < 4) {
+    document.querySelector('.scoreText1').innerHTML = 'PÉSSIMO'
+    document.querySelector('.scoreText1').style.color = 'red'
+    document.querySelector('.scorePct').innerHTML = `Você acertou ${points}%`
+
+    document.querySelector('.scorePct').style.color = 'yellow'
+  }
+}
+
+document.querySelector('button').addEventListener('click', returnInitial)
+
+function returnInitial() {
+  currentQuestion = 0
+  correctAnswer = 0
+  document.querySelector('.scoreArea').style.display = 'none'
+  document.querySelector('.questionArea').style.display = 'block'
+  document.querySelector('.progress--bar').style.width = '0%'
+}
